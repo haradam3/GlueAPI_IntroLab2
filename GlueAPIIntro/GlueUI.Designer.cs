08c3297db93ec158a5a0102cb5b7287508944e20
haradam3 <mikako.harada@autodesk.com>
2018-01-03 15:46:04 -0500

    Initial commit

---
+++ GlueAPIIntro/GlueUI.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace GlueAPIIntro
+{
+    partial class GlueUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.labelResponse = new System.Windows.Forms.Label();
+            this.textBoxResponse = new System.Windows.Forms.TextBox();
+            this.labelRequest = new System.Windows.Forms.Label();
+            this.textBoxRequest = new System.Windows.Forms.TextBox();
+            this.buttonProjects = new System.Windows.Forms.Button();
+            this.buttonModels = new System.Windows.Forms.Button();
+            this.buttonView = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.labelCurProj = new System.Windows.Forms.Label();
+            this.labelCurModel = new System.Windows.Forms.Label();
+            this.labelUserName = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.comboBoxProjects = new System.Windows.Forms.ComboBox();
+            this.comboBoxModels = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(406, 37);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(90, 25);
+            this.buttonLogin.TabIndex = 1;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // labelResponse
+            // 
+            this.labelResponse.AutoSize = true;
+            this.labelResponse.Location = new System.Drawing.Point(27, 338);
+            this.labelResponse.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelResponse.Name = "labelResponse";
+            this.labelResponse.Size = new System.Drawing.Size(72, 17);
+            this.labelResponse.TabIndex = 2;
+            this.labelResponse.Text = "Response";
+            // 
+            // textBoxResponse
+            // 
+            this.textBoxResponse.Location = new System.Drawing.Point(27, 363);
+            this.textBoxResponse.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxResponse.Multiline = true;
+            this.textBoxResponse.Name = "textBoxResponse";
+            this.textBoxResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResponse.Size = new System.Drawing.Size(512, 209);
+            this.textBoxResponse.TabIndex = 3;
+            // 
+            // labelRequest
+            // 
+            this.labelRequest.AutoSize = true;
+            this.labelRequest.Location = new System.Drawing.Point(27, 240);
+            this.labelRequest.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelRequest.Name = "labelRequest";
+            this.labelRequest.Size = new System.Drawing.Size(61, 17);
+            this.labelRequest.TabIndex = 4;
+            this.labelRequest.Text = "Request";
+            // 
+            // textBoxRequest
+            // 
+            this.textBoxRequest.Location = new System.Drawing.Point(27, 265);
+            this.textBoxRequest.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxRequest.Multiline = true;
+            this.textBoxRequest.Name = "textBoxRequest";
+            this.textBoxRequest.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxRequest.Size = new System.Drawing.Size(512, 61);
+            this.textBoxRequest.TabIndex = 5;
+            // 
+            // buttonProjects
+            // 
+            this.buttonProjects.Location = new System.Drawing.Point(406, 145);
+            this.buttonProjects.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonProjects.Name = "buttonProjects";
+            this.buttonProjects.Size = new System.Drawing.Size(90, 25);
+            this.buttonProjects.TabIndex = 6;
+            this.buttonProjects.Text = "Projects";
+            this.buttonProjects.UseVisualStyleBackColor = true;
+            this.buttonProjects.Click += new System.EventHandler(this.buttonProjects_Click);
+            // 
+            // buttonModels
+            // 
+            this.buttonModels.Location = new System.Drawing.Point(406, 186);
+            this.buttonModels.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonModels.Name = "buttonModels";
+            this.buttonModels.Size = new System.Drawing.Size(90, 25);
+            this.buttonModels.TabIndex = 7;
+            this.buttonModels.Text = "Models";
+            this.buttonModels.UseVisualStyleBackColor = true;
+            this.buttonModels.Click += new System.EventHandler(this.buttonModels_Click);
+            // 
+            // buttonView
+            // 
+            this.buttonView.Location = new System.Drawing.Point(567, 547);
+            this.buttonView.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonView.Name = "buttonView";
+            this.buttonView.Size = new System.Drawing.Size(90, 25);
+            this.buttonView.TabIndex = 8;
+            this.buttonView.Text = "View";
+            this.buttonView.UseVisualStyleBackColor = true;
+            this.buttonView.Click += new System.EventHandler(this.buttonView_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(567, 15);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(4);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(27, 25);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(525, 513);
+            this.webBrowser1.TabIndex = 9;
+            // 
+            // labelCurProj
+            // 
+            this.labelCurProj.AutoSize = true;
+            this.labelCurProj.Location = new System.Drawing.Point(23, 142);
+            this.labelCurProj.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelCurProj.Name = "labelCurProj";
+            this.labelCurProj.Size = new System.Drawing.Size(52, 17);
+            this.labelCurProj.TabIndex = 10;
+            this.labelCurProj.Text = "Project";
+            // 
+            // labelCurModel
+            // 
+            this.labelCurModel.AutoSize = true;
+            this.labelCurModel.Location = new System.Drawing.Point(24, 190);
+            this.labelCurModel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelCurModel.Name = "labelCurModel";
+            this.labelCurModel.Size = new System.Drawing.Size(46, 17);
+            this.labelCurModel.TabIndex = 11;
+            this.labelCurModel.Text = "Model";
+            // 
+            // labelUserName
+            // 
+            this.labelUserName.AutoSize = true;
+            this.labelUserName.Location = new System.Drawing.Point(24, 43);
+            this.labelUserName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelUserName.Name = "labelUserName";
+            this.labelUserName.Size = new System.Drawing.Size(79, 17);
+            this.labelUserName.TabIndex = 12;
+            this.labelUserName.Text = "User Name";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(24, 88);
+            this.labelPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(69, 17);
+            this.labelPassword.TabIndex = 13;
+            this.labelPassword.Text = "Password";
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(120, 40);
+            this.textBoxUserName.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(268, 22);
+            this.textBoxUserName.TabIndex = 14;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(123, 85);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(268, 22);
+            this.textBoxPassword.TabIndex = 15;
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Enabled = false;
+            this.buttonLogout.Location = new System.Drawing.Point(406, 82);
+            this.buttonLogout.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(90, 25);
+            this.buttonLogout.TabIndex = 16;
+            this.buttonLogout.Text = "Logout";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(120, 337);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(0, 17);
+            this.labelStatus.TabIndex = 17;
+            // 
+            // comboBoxProjects
+            // 
+            this.comboBoxProjects.FormattingEnabled = true;
+            this.comboBoxProjects.Location = new System.Drawing.Point(123, 142);
+            this.comboBoxProjects.Name = "comboBoxProjects";
+            this.comboBoxProjects.Size = new System.Drawing.Size(268, 24);
+            this.comboBoxProjects.TabIndex = 18;
+            this.comboBoxProjects.SelectedIndexChanged += new System.EventHandler(this.comboBoxProjects_SelectedIndexChanged);
+            // 
+            // comboBoxModels
+            // 
+            this.comboBoxModels.FormattingEnabled = true;
+            this.comboBoxModels.Location = new System.Drawing.Point(123, 187);
+            this.comboBoxModels.Name = "comboBoxModels";
+            this.comboBoxModels.Size = new System.Drawing.Size(268, 24);
+            this.comboBoxModels.TabIndex = 19;
+            // 
+            // GlueUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 591);
+            this.Controls.Add(this.comboBoxModels);
+            this.Controls.Add(this.comboBoxProjects);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUserName);
+            this.Controls.Add(this.labelCurModel);
+            this.Controls.Add(this.labelCurProj);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.buttonView);
+            this.Controls.Add(this.buttonModels);
+            this.Controls.Add(this.buttonProjects);
+            this.Controls.Add(this.textBoxRequest);
+            this.Controls.Add(this.labelRequest);
+            this.Controls.Add(this.textBoxResponse);
+            this.Controls.Add(this.labelResponse);
+            this.Controls.Add(this.buttonLogin);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "GlueUI";
+            this.Text = "Glue API Intro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label labelResponse;
+        private System.Windows.Forms.TextBox textBoxResponse;
+        private System.Windows.Forms.Label labelRequest;
+        private System.Windows.Forms.TextBox textBoxRequest;
+        private System.Windows.Forms.Button buttonProjects;
+        private System.Windows.Forms.Button buttonModels;
+        private System.Windows.Forms.Button buttonView;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label labelCurProj;
+        private System.Windows.Forms.Label labelCurModel;
+        private System.Windows.Forms.Label labelUserName;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonLogout;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.ComboBox comboBoxProjects;
+        private System.Windows.Forms.ComboBox comboBoxModels;
+    }
+}
+
---
+++ GlueAPIIntro/GlueUI.cs
@@ -0,0 +1,204 @@
+﻿#region Copyright
+//
+// Copyright (C) 2013-2018 by Autodesk, Inc.
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+//
+// Use, duplication, or disclosure by the U.S. Government is subject to
+// restrictions set forth in FAR 52.227-19 (Commercial Computer
+// Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
+// (Rights in Technical Data and Computer Software), as applicable.
+//
+// Written by M.Harada 
+#endregion // Copyright
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+// Added for RestSharp. 
+using RestSharp;
+
+namespace GlueAPIIntro
+{
+
+    public partial class GlueUI : Form
+    {
+        // Save info needed. 
+        private static string m_authToken = "";
+        private static List<Project> m_proj_list;
+        private static List<ModelInfo> m_model_list;
+
+        public GlueUI()
+        {
+            InitializeComponent();
+        }
+
+        //=========================================================
+        //  Login/Logout
+        //=========================================================
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            // Get the user name and password from the user. 
+            string userName = textBoxUserName.Text;
+            string password = textBoxPassword.Text;
+
+            InitRequestResponse();
+            this.Update();
+
+            // Here is the main part that we call Glue login 
+            m_authToken = Glue.Login(userName, password);
+
+            // If success, change the button to logout.
+            if (m_authToken != null && m_authToken.Length > 0)
+            {
+                buttonLogin.Enabled = false;
+                buttonLogout.Enabled = true;
+            }
+
+            // Show the request and response in the form. 
+            // This is for learning purpose. 
+            ShowRequestResponse();
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            InitRequestResponse();
+            this.Update();
+
+            // Here is the main part that we call Glue login 
+            bool logoutResponse = Glue.Logout(m_authToken);
+
+            m_authToken= "";
+            buttonLogin.Enabled = true;
+            buttonLogout.Enabled = false;
+
+            // Show the request and response in the form. 
+            // This is for learning purpose. 
+            ShowRequestResponse();
+        }
+
+        //==========================================================
+        // Helper functions 
+        //==========================================================
+        private void InitRequestResponse()
+        {
+            // initialize the request and response text in the form.
+            textBoxRequest.Text = "Request comes here";
+            labelStatus.Text = "";
+            textBoxResponse.Text = "Response comes here. This may take seconds. Please wait...";
+        }
+        private void ShowRequestResponse()
+        {
+            // show the request and response in the form. 
+            IRestResponse response = Glue.m_lastResponse;
+            textBoxRequest.Text = response.ResponseUri.AbsoluteUri;
+            labelStatus.Text = "Status: " + response.StatusCode.ToString();
+            textBoxResponse.Text = response.Content;
+        }
+
+        //=========================================================
+        //  Projects 
+        //=========================================================
+        private void buttonProjects_Click(object sender, EventArgs e)
+        {
+            InitRequestResponse();
+            this.Update();
+
+            m_proj_list = Glue.ProjectList(m_authToken);
+
+            ShowRequestResponse();
+
+            if (m_proj_list == null) { return; }
+
+            // Set up a project list
+            m_proj_list = m_proj_list.OrderBy(x => x.project_name).ToList();
+            comboBoxProjects.DataSource = new BindingSource(m_proj_list, null);
+            comboBoxProjects.DisplayMember = "project_name";
+            comboBoxProjects.ValueMember = "project_id";
+            comboBoxProjects.SelectedIndex = 0;
+        }
+
+        private void comboBoxProjects_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // clear model list
+            comboBoxModels.DataSource = null; 
+        }
+
+        //=========================================================
+        //  Models 
+        //=========================================================
+        private void buttonModels_Click(object sender, EventArgs e)
+        {
+            InitRequestResponse();
+            this.Update();
+
+            Project proj = (Project)comboBoxProjects.SelectedItem;
+            string project_id = proj.project_id;
+
+            m_model_list = Glue.ModelList(m_authToken, project_id);
+
+            // Show the request and response in the form. 
+            // This is for learning purpose. 
+            ShowRequestResponse();
+
+            if (m_model_list == null) { return; }
+
+            // Set up a model list
+            m_model_list = m_model_list.OrderBy(x => x.model_name).ToList();
+            comboBoxModels.DataSource = new BindingSource(m_model_list, null);
+            comboBoxModels.DisplayMember = "model_name";
+            comboBoxModels.ValueMember = "model_id";
+            comboBoxModels.SelectedIndex = 0;
+        }
+
+        //=========================================================
+        // Viewer
+        // July 2016: deplicated.  
+        // https://fieldofviewblog.wordpress.com/2015/05/06/google-chrome-drops-npapi-plugin-support/ 
+        // TBD: maybe in future we will extend this portion to use Forge Viewer. 
+        // If we do, however, it will be an advanced lab and not as intro. 
+        //=========================================================
+        private void buttonView_Click(object sender, EventArgs e)
+        {
+            InitRequestResponse();
+            this.Update();
+
+            Project proj = (Project)comboBoxProjects.SelectedItem;
+            string project_id = proj.project_id;
+            ModelInfo model = (ModelInfo)comboBoxModels.SelectedItem;
+            string model_id = model.model_id;
+
+            // The response we get here is URL that we embed in iframe. 
+            // Let's see what we got by showing in the response text box.
+            // If you take this string and copy&paste in a simple html 
+            // file with an ifarme, you will see a model.
+            // Note: viewer is not supported in windows control.
+            // ScriptErrorsSuppressed is set to true to suppress script errors. 
+            // We are using it here to learn web services API. 
+
+            string url = Glue.View(m_authToken, project_id, model_id);
+
+            textBoxRequest.Text = url;
+            textBoxResponse.Text = "displaying model...";
+            this.Update();
+
+            // a view embedded form's web browser by URL.
+            webBrowser1.Url = new System.Uri(url);
+
+        }
+
+    }
+}
